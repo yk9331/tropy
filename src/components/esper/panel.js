17c3e4b757ff3ce216a11e916e4587d24783306c
Sylvester Keil <sylvester@keil.or.at>
2020-06-25 10:51:32 +0200

    Adjust balance filter ranges

--- src/components/esper/panel.js
+++ src/components/esper/panel.js
@@ -119,8 +119,8 @@ const EsperPanel = (props) => {
         <ColorSlider
           icon={<IconSharpen/>}
           isDisabled={props.isDisabled || !props.isVisible}
-          min={-127}
-          max={127}
+          min={-100}
+          max={100}
           tabIndex={TABS.EsperPanel}
           type="aBalance"
           value={props.aBalance}
@@ -128,8 +128,8 @@ const EsperPanel = (props) => {
         <ColorSlider
           icon={<IconSharpen/>}
           isDisabled={props.isDisabled || !props.isVisible}
-          min={-127}
-          max={127}
+          min={-100}
+          max={100}
           tabIndex={TABS.EsperPanel}
           type="bBalance"
           value={props.bBalance}
--- src/esper/filter/balance.js
+++ src/esper/filter/balance.js
@@ -7,6 +7,10 @@ const frag = res.shader.load('balance.frag')
 
 
 class BalanceFilter extends PIXI.Filter {
+  #max = 100
+  #min = 100
+  #precision = 0.0032
+
   constructor(a = 0, b = 0) {
     super(undefined, frag)
     this.a = a
@@ -18,7 +22,7 @@ class BalanceFilter extends PIXI.Filter {
   }
 
   set a(value) {
-    this.uniforms.a = restrict(value, -127, 127) / 100
+    this.uniforms.a = this.restrict(value)
   }
 
   get b() {
@@ -26,12 +30,16 @@ class BalanceFilter extends PIXI.Filter {
   }
 
   set b(value) {
-    this.uniforms.b = restrict(value, -127, 127) / 100
+    this.uniforms.b = this.restrict(value)
   }
 
   set(a, b) {
     this.a = a
     this.b = b
+  }
+
+  restrict(value) {
+    return restrict(value, this.#min, this.#max) * this.#precision
   }
 }
 
